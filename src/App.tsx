595814589c7697f6952cbe2f97b89db0533ca7c8
AngelSiete <andraco660@gmail.com>
2024-09-17 15:38:09 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
+import Todos from './components/todos';
+const dummyTodos = ['Learn React', 'Learn TypeScript', 'Learn JavaScript'];
 
 function App() {
   return (
     <div className="App">
-      
+      <Todos items={dummyTodos} />
     </div>
   );
 }
