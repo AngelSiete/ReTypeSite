58ea4721647f9636b87043067f185913b5747a11
AngelSiete <andraco660@gmail.com>
2024-09-18 16:58:34 +0200

    state manag and styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,20 @@ import './App.css';
 import NewTodo from './components/NewTodo';
 import Todos from './components/todos';
 import Todo from './models/todo';
+import { useState } from 'react';
+
 function App() {
-  const todos = [new Todo('Learn React'), new Todo('Learn TypeScript'), new Todo('Learn JavaScript')];
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAddTodo = (text: string) => {
+    const newTodo = new Todo(text);
+    setTodos((prevTodos) => [...prevTodos, newTodo]);
+  };
+  
   return (
     <div className="App">
       <Todos items={todos} />
-      <NewTodo />
+      <NewTodo onAddTodo={handleAddTodo} />
     </div>
   );
 }
--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,17 +1,18 @@
 import { useRef } from "react";
-
-const NewTodo = () => {
+import classes from './NewTodo.module.css';
+const NewTodo: React.FC<{onAddTodo: (text: string) => void}> = (props) => {
     const textInputRef = useRef<HTMLInputElement>(null);
     const handleSubmit = (event: React.FormEvent) => {
         event.preventDefault();
         const enteredText = textInputRef.current!.value;
         if (enteredText.trim().length === 0) {
             return;
-        }
-        
+        };
+        props.onAddTodo(enteredText);
+        textInputRef.current!.value = '';
     }
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className={classes.form}>
         <div>
             <label htmlFor="text">Todo Text</label>
             <input type="text" id="text" ref={textInputRef} required />
